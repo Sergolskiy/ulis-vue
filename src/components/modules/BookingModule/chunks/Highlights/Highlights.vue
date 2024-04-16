cb38886bed458735e9d9454778de43981996fe17
Sergolskiy <sergolskiy@gmail.com>
2024-04-16 16:50:15 +0200

    developing

---
+++ src/components/modules/BookingModule/chunks/Highlights/Highlights.vue
@@ -0,0 +1,41 @@
+<template>
+ 
+  <div class="highlights">
+
+    <Highlight 
+           v-for="(highlight, index) in highlights"
+          :key="index"
+          :highlight="highlight"
+    />
+  </div>
+</template>
+
+<script>
+import Highlight from '../Highlights/Highlight.vue'
+
+export default {
+  name: "Highlights",
+  components: {
+    Highlight
+  },
+
+  props: {
+    highlights: {
+      type: Array,
+      default: []
+    }
+  },
+
+  data: function () {
+    return {
+
+    };
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import "../../../../../scss/colors";
+@import "../../../../../scss/mixins/mixins";
+
+</style>
